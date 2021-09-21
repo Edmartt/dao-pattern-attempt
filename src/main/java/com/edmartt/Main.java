c293e9944c99f267c376b5333c2ad2d77b178aff
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-21 14:39:17 -0500

    Added a line for tables of sqlite db manager and changed the implementation sent to Userquerys

--- src/main/java/com/edmartt/Main.java
+++ src/main/java/com/edmartt/Main.java
@@ -4,12 +4,15 @@ import com.edmartt.repository.IDao;
 import com.edmartt.repository.user_querys.UserQuerys;
 import com.edmartt.users.Users;
 import com.edmartt.repository.connector.Mariadb;
+import com.edmartt.repository.connector.SQLiteConnector;
+import com.edmartt.repository.connector.SQLiteTables;
 
 public class Main {
 
     public static void main(String... args){
         Users user = new Users("Test", "Data", "Git", "mail@mail.com");
-        IDao querys = new UserQuerys(new Mariadb());
+        new SQLiteTables(new SQLiteConnector()).createTable(); // Crea tablas en sqlite
+        IDao querys = new UserQuerys(new SQLiteConnector());
         querys.add(user);
     }
 }
